b2bef67762b10b4784bcae57a8b11ac7e1859e5a
Kerry Smart <kerry.z.smart@gmail.com>
2022-04-13 19:08:39 -0400

    basic functionality done: select number of colours, shade (optional), display as squares, and clear the board

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
-function getFetch(){
-  console.log(choice);
+document.querySelector("button").addEventListener("click", getColours);
+document.querySelector("button + button").addEventListener("click", clearColours);
 
-  const url = "";
+const colourBox = document.querySelector(".colour-box");
+
+function getColours(){
+  let n = document.querySelector("input").value;
+  let c = document.querySelector("select").value;
+
+  const url = `https://x-colors.herokuapp.com/api/random/${c}?number=${n}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
+      .then(res => res.json()) 
       .then(data => {
-        console.log(data)
+        console.log(data);
+        console.log(typeof(data))
+
+        if (Array.isArray(data)) {
+          for (let colour of data) {
+            let newDiv = document.createElement("div");
+            newDiv.style.backgroundColor = colour.hex;
+            colourBox.appendChild(newDiv);
+          }
+        } else {
+          let newDiv = document.createElement("div");
+          newDiv.style.backgroundColor = data.hex;
+          colourBox.appendChild(newDiv);
+        }
+
       })
       .catch(err => {
           console.log(`error ${err}`);
       });
 }
 
+function clearColours() {
+  while (colourBox.firstChild) {
+    colourBox.removeChild(colourBox.lastChild);
+  }
+}
